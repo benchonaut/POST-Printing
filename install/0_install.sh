38537b0800a35ef84206303c33d2c42161b5301e
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-11-23 11:30:58 +0100

    2017-11-23_10:30

--- install/0_install.sh
+++ install/0_install.sh
@@ -22,7 +22,7 @@ cp -aurv printer_status.sh printer_clean_tmp.sh /etc/
 cp -aurv print.php router.php /var/www/html/
 chown -R www-data:www-data /var/www/
 chown -R root:lp /etc/cups
-grep printer_clean_tmp /etc/crontab  || (echo Installing cron cleanr; echo "Ki81ICoJKiAqICoJbHAJL2Jpbi9iYXNoIC9ldGMvcHJpbnRlcl9jbGVhbl90bXAuc2gKCg=="|base64 -d |tee -a /etc/crontab )
+grep printer_clean_tmp /etc/crontab  || (echo Installing cron cleanr; echo "Ki81ICoJKiAqICoJcm9vdAkvYmluL2Jhc2ggL2V0Yy9wcmludGVyX2NsZWFuX3RtcC5zaAoK"|base64 -d |tee -a /etc/crontab )
 
 (test -e /etc/ssl/private/nginx.key && test -e /etc/ssl/private/crt.pem ) ||   openssl req -x509 -nodes -days 3650 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=printserver.local" -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/private/crt.pem
 ln -s /etc/ssl/private/crt.pem /etc/ssl/private/ca.pem
