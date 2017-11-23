5d61cc2f9099d63bc4f1f67c028a937cd574e10a
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-11-23 11:17:18 +0100

    2017-11-23_10:17

--- install/printer_clean_tmp.sh
+++ install/printer_clean_tmp.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-
+#!/bin/bash
+declare -x PATH="/home/andosto/bin:/home/andosto/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 find -type f /tmp/ -name "br_lpdfilter_ink_gsout_*" |while read file;do age=$(expr $(date +%s ) - $(date -u +%s -r $file));echo $age;if [ "$age" -gt 3600 ];then rm "$file" ;fi  & done
