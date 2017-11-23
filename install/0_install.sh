c69966b6ec0ee80a091d42f6e465cc07d1d24206
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-11-23 12:36:21 +0100

    2017-11-23_11:36

--- install/0_install.sh
+++ install/0_install.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get -y --force-yes install sshfs git pv snmp nginx curl ph
 dpkg -i --force-all ql720nwlpr-1.1.4-0.i386.deb && dpkg -i --force-all ql720nwcupswrapper-1.1.4-0.i386.deb
 cp evolis-primacyE.ppd.gz /usr/share/cups/model/ && cp evorasterizer /usr/lib/cups/filter/ && chmod 755 /usr/share/cups/model/evolis-primacyE.ppd.gz /usr/lib/cups/filter/evorasterizer
 
-mkdir /var/lib/tor/ssh;chown debian-tor:debian-tor /var/lib/tor/ssh;grep "^HiddenServiceDir /var/lib/tor/ssh/" /etc/tor/torrc || (echo "CkhpZGRlblNlcnZpY2VEaXIgL3Zhci9saWIvdG9yL3NzaC8KSGlkZGVuU2VydmljZVBvcnQgMjIgMTI3LjAuMC4xOjIyCkhpZGRlblNlcnZpY2VBdXRob3JpemVDbGllbnQgc3RlYWx0aCBzc2gKCgo"|base64 -d  >> /etc/tor/torrc ;/etc/init.d/tor restart 2>&1 > /dev/null & )
+mkdir /var/lib/tor/ssh;chown debian-tor:debian-tor /var/lib/tor/ssh;grep "^HiddenServiceDir /var/lib/tor/ssh/" /etc/tor/torrc || (echo "CkhpZGRlblNlcnZpY2VEaXIgL3Zhci9saWIvdG9yL3NzaC8KSGlkZGVuU2VydmljZVBvcnQgMjIgMTI3LjAuMC4xOjIyCkhpZGRlblNlcnZpY2VBdXRob3JpemVDbGllbnQgc3RlYWx0aCBzc2gKCgo"|base64 -d  >> /etc/tor/torrc )
 (grep "^PermitRootLogin without-password" /etc/ssh/sshd_config -q || grep "^PermitRootLogin prohibit-password" /etc/ssh/sshd_config -q) && echo SAFE || ( echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config )
 
 for base in *base64 ; do  base64 -d $base > "/etc/cups/ppd/"${base/.base64/};done
@@ -42,6 +42,8 @@ echo "DON'T FORGET TO DEPLOY SSL KEYS UNDER /etc/ssl/private/nginx.key AND /etc/
 echo "DON'T FORGET TO DEPLOY SSL KEYS UNDER /etc/ssl/private/nginx.key AND /etc/ssl/private/crt.pem"
 echo "DON'T FORGET TO DEPLOY SSL KEYS UNDER /etc/ssl/private/nginx.key AND /etc/ssl/private/crt.pem"
 wait 
+chmod go-rwx /var/lib/tor/ssh/
+/etc/init.d/tor restart 2>&1 > /dev/null 
 echo REMOTE: $(cat /var/lib/tor/ssh/hostname|cut -d"." -f1)
 echo
 echo DONE
