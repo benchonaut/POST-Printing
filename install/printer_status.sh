c1888365d2fc812001161397b4762572805dfbfe
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-11-23 10:32:50 +0100

    2017-11-23_09:32

--- install/printer_status.sh
+++ install/printer_status.sh
@@ -4,7 +4,7 @@ primacy_stat()	{ ping -c1 -w2 -D $1|grep -q "bytes from" && (res=$(wget -q -O- h
 					res=$(echo "$res"|grep Firmware|cut -d">" -f3,5) ; res=${res//td/} ; res=${res/\<\//} ;res=${res/\//}; echo "|"$res;) |tr -d '\n' ;  } ; 
 ql720_stat()	{ ping -c1 -w2 -D $1|grep -q "bytes from" && (echo -n $(snmpwalk -v2c -c public $1 iso.3.6.1.2.1.43.16.5.1.2.1.1|cut -d":" -f2 ;
 					echo "|TotalPages:" ;snmpwalk -v2c -c public $1 iso.3.6.1.2.1.43.10.2.1.4.1.1|cut -d":" -f2  ;
-					echo "|Paper";snmpwalk -v2c -c public $1 iso.3.6.1.2.1.43.8.2.1.12.1.1|sed 's/"//g' ) ) ;  } ; 
+					echo "|Paper";snmpwalk -v2c -c public $1 iso.3.6.1.2.1.43.8.2.1.12.1.1 |cut -d":" -f2|sed 's/"//g' ) ) ;  } ; 
 replace() { sed 's/^/'$1'/g;s///g' ; } ;
 
 for CARD in $(lpstat -s|grep CARD|cut -d: -f1,3|sed 's/^.\+CARD//g');do
