2a47a393080ef3d0b8fbcc09d79f5b37c160b25c
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2017-11-27 17:06:51 +0100

    2017-11-27_16:06

--- install/router.php
+++ install/router.php
@@ -69,7 +69,10 @@ if(isset($_POST) AND !empty($_POST))
 //file_put_contents('/tmp/printrouterCONF.log', print_r(count((array)$config))); //DEBUG...DUMP config object count
 //station id is determined by last number of ipv4
 print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html><head><title>Printer Selector '.curPageURL().'</title></head><body><br><hr>Printer Routing</h1>');
-print('<form method="POST" action="'.curPageURL().'?action=RotFront" onchange="document.getElementById(\'RotFront\').form.submit()"> <button name="RotFront" id="RotFront" value="upvote">Rotate Front</button></form>');
+print('<br>Card Rotation(all printers)');
+print('<form method="POST" action="'.curPageURL().'?action=NoRotFront" onchange="document.getElementById(\'NoRotFront\').form.submit()"> <button name="NoRotFront" id="RotOff" value="Front">Straight Front</button></form>');
+print('<form method="POST" action="'.curPageURL().'?action=RotFront" onchange="document.getElementById(\'RotFront\').form.submit()"> <button name="RotFront" id="RotOn" value="Front">Rotate Front</button></form>');
+
 
 print('<table><tr><th>Station</th><th>Card<br>Printer</th><th>Card<br>Status</th><th>Label<br>Printer</th><th>Label<br>Status</th></tr><tr>');
    for($station=1; $station < count((array)$config) + 1 ; $station++) {
