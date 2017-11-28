1fd4d2a393f441852abaebc3a57c00eb04f6e116
Benjamin Freitag <benjamin.freitag@kosmoskosmos.de>
2017-11-28 02:21:36 +0100

    2017-11-28-01-21

--- install/printer_clean_tmp.sh
+++ install/printer_clean_tmp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-declare -x PATH="/home/andosto/bin:/home/andosto/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+declare -x PATH="~/bin:~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 find /tmp/ -type f -name "br_lpdfilter_ink_gsout_*" |while read file;do age=$(expr $(date +%s ) - $(date -u +%s -r $file));if [ "$age" -gt 3600 ];then chown $(id -un) $file ; rm "$file" ;fi  & done
