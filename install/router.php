73311885749e463b2bae60a147e49ca8222873c6
Benjamin Freitag <benjamin.freitag@kosmoskosmos.de>
2017-11-28 04:21:19 +0100

    2017-11-28-03-21

--- install/router.php
+++ install/router.php
@@ -152,24 +152,24 @@ td:first-child, th:first-child {
 </head><body><h3>Printer Routing</h3>
 ');
 
-
 print('<hr>Card Rotation(all printers):<br><table align=center><tr><th>');
 print('<form method="POST" action="'.curPageURL().'?action=NoRotFront" onchange="document.getElementById(\'NoRotFront\').form.submit();history.go(0);"> <button name="NoRotate" id="NoRotFront" value="Front">Straight Front</button></form>');
 print('</th><th><form method="POST" action="'.curPageURL().'?action=RotFront" onchange="document.getElementById(\'RotFront\').form.submit();history.go(0);"> <button name="Rotate" id="RotFront" value="Front">Rotate Front 180°</button></form>');
 print('</th><th><form method="POST" action="'.curPageURL().'?action=NoRotBack" onchange="document.getElementById(\'NoRotBack\').form.submit();history.go(0);"> <button name="NoRotate" id="NoRotBack" value="Back">Straight Back</button></form>');
 print('</th><th><form method="POST" action="'.curPageURL().'?action=RotBack" onchange="document.getElementById(\'RotBack\').form.submit();history.go(0);"> <button name="Rotate" id="RotBack" value="Back">Rotate Back 180°</button></form>');
 print('</th></tr></table>');
-print('<hr><table><tr><th>Station</th><th>Card<br>Printer</th><th>Card Status(Printer Number)</th><th>Label<br>Printer</th><th>Label Status(Printer Number)</th></tr><tr>');
+print('<hr><table><tr><th>Station/Printer</th><th>Card<br>Printer</th><th>Card Status(Printer Number)</th><th>Label<br>Printer</th><th>Label Status(Printer Number)</th></tr><tr>');
    for($station=1; $station < count((array)$config) + 1 ; $station++) {
 		print('<td>'.$station.'</td>');
-		print('<td><form method="POST" action="'.curPageURL().'?action=card" onchange="document.getElementById(\'card_'.$station.'\').form.submit()"> <select id=card_'.$station.'  name=card_'.$station.'  required><option selected>'.getCardNum($config,$station)); 
-			for($i=1; $i < count((array)$config) + 1; $i++) { print('<option>'.$i); }
-		print('</select></form></td>');
+		print('<td>S'.$station.'<form method="POST" action="'.curPageURL().'?action=card" onchange="document.getElementById(\'card_'.$station.'\').form.submit()"> <select id=card_'.$station.'  name=card_'.$station.'  required><option selected>'.getCardNum($config,$station)); 
+				for($i=1; $i < count((array)$config) + 1; $i++) { print('<option>'.$i); }
+			print('</select></form></td>');
 		if (isset($status['card-'.sprintf("%02d",$station)])) { print('<td>'.$status['card-'.sprintf("%02d",$station)].'</td>'); }
 			else { print('<td >..</td>'); }
-		print('<td><form method="POST" action="'.curPageURL().'?action=label" onchange="document.getElementById(\'label_'.$station.'\').form.submit()"><select id=label_'.$station.' name=label_'.$station.' required><option selected>'.getLabelNum($config,$station));
-			for($i=1; $i < count((array)$config) + 1 ; $i++) { print('<option>'.$i); }
-		print('</select></form></td>');
+		print('<td>S'.$station.'<form method="POST" action="'.curPageURL().'?action=label" onchange="document.getElementById(\'label_'.$station.'\').form.submit()"><select id=label_'.$station.' name=label_'.$station.' required><option selected>'.getLabelNum($config,$station));
+				for($i=1; $i < count((array)$config) + 1 ; $i++) { print('<option>'.$i); }
+			print('</select></form></td>');
+		
 		if (isset($status['label-'.sprintf("%02d",$station)])) { print('<td>'.$status['label-'.sprintf("%02d",$station)].'</td>'); }
 			else { print('<td >..</td>'); }		
 		print('</tr>');
