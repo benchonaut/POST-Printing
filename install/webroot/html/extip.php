e7a723b406a2145bf6a94b891ec034ca3074f99d
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-05-11 23:46:39 +0200

    live-status, keep-printer-status-files, added alpine install routines

--- install/webroot/html/extip.php
+++ install/webroot/html/extip.php
@@ -3,5 +3,5 @@
 $output=array();
 exec(' nslookup  myip.opendns.com resolver1.opendns.com|grep -A3 "^Name:"|grep ^Add|cut -d " " -f2|while read addr;do bash /etc/extip.sh $addr;done',$output);
 //print(implode("\n",$output)."\n");
-print('<table class="mytable darktable"><thead><td class="mytd"><p style="color:red;font-size: 15px">Connection Status </p></td></thead><tr class="mytr"><td class="mytd"><code>'.implode('</code></td></tr><tr class="mytr"><td class="mytd"><code>',$output).'</code></td></tr><tr><td><p style="color:red;font-size: 10px;margin 1px">( reload every 10 min)</p></td></tr></table><br>');
+print('<table class="mytable darktable"><thead><td class="mytd"><p style="color:red;font-size: 15px">Connection Status </p></td></thead><tr class="mytr"><td class="mytd"><code>'.implode('</code></td></tr><tr class="mytr"><td class="mytd"><code>',$output).'</code></td></tr><tr><td><div style="color:red;font-size: 10px;margin 1px">( reload every 10 min)</div></td></tr></table><br>');
 unset($output);
