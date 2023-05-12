2a4d8dd14541b7d007bf51f8115a4ee2504cad1b
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-05-12 17:33:21 +0200

    live-status, keep-printer-status-files, added alpine install routines

--- install/0_install.sh
+++ install/0_install.sh
@@ -28,7 +28,7 @@ test -e /etc/localtime || ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtim
 (
 echo '#!/bin/bash
 
-test -e /dev/shm/printer-events.log||mkdir  /dev/shm/printer-events.log ; python3 -u /usr/bin/pycups-notify 2>&1|websocat --websocket-ignore-zeromsg --async-stdio ws://192.168.88.254:11111/ |while read line ;do find /dev/shm/printer-events.log/ -mmin +30 -type f -delete &>/dev/null &  echo $line >  /dev/shm/printer-events.log/printer-events.$(date +%F_%H.%M);   done ' > /etc/run.cups.notify.sh
+test -e /dev/shm/printer-events.log||mkdir  /dev/shm/printer-events.log ; python3 -u $(which pycups-notify) 2>&1|websocat --websocket-ignore-zeromsg --async-stdio ws://192.168.88.254:11111/ |while read line ;do find /dev/shm/printer-events.log/ -mmin +30 -type f -delete &>/dev/null &  echo $line >  /dev/shm/printer-events.log/printer-events.$(date +%F_%H.%M);   done ' > /etc/run.cups.notify.sh
 
 
 ) &
--- install/scripts/run.cups.notify.sh
+++ install/scripts/run.cups.notify.sh
@@ -3,7 +3,9 @@ trap 'kill $(jobs -p)' EXIT
 trap 'pkill -P $$' SIGINT SIGTERM
 
 test -e /dev/shm/printer-events.log||mkdir  /dev/shm/printer-events.log ; 
-python3 -u /usr/bin/pycups-notify 2>&1|websocat --websocket-ignore-zeromsg --async-stdio ws://192.168.88.254:11111/ |while read line ;do 
+which pycups-notify &>/dev/null || ( sleep 3;echo "pycups-notify not found")
+which pycups-notify &>/dev/null || exit 1
+python3 -u $( which pycups-notify) 2>&1|websocat --websocket-ignore-zeromsg --async-stdio ws://192.168.88.254:11111/ |while read line ;do 
   find /dev/shm/printer-events.log/ -mmin +30 -type f -delete &>/dev/null &  
   echo $line >>  /dev/shm/printer-events.log/printer-events.$(date -u  +%F_%H.%M);
 done 
