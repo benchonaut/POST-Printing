1d56cb4b18bfd9bb1e986b7f117d474056670e59
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-05-24 15:55:32 +0200

    live-status, keep-printer-status-files, added alpine install routines

--- install/scripts/run.cups.notify.sh
+++ install/scripts/run.cups.notify.sh
@@ -5,7 +5,10 @@ trap 'pkill -P $$' SIGINT SIGTERM
 test -e /dev/shm/printer-events.log||mkdir  /dev/shm/printer-events.log ; 
 which pycups-notify &>/dev/null || ( sleep 3;echo "pycups-notify not found")
 which pycups-notify &>/dev/null || exit 1
+
+while (true);do sleep 42;find /dev/shm/printer-events.log/ -mmin +30 -type f -delete &>/dev/null ;done &
+
 python3 -u $( which pycups-notify) 2>&1| bash /etc/cups-notify-filter.sh | websocat --websocket-ignore-zeromsg --async-stdio ws://192.168.88.254:11111/ |while read line ;do 
-  find /dev/shm/printer-events.log/ -mmin +30 -type f -delete &>/dev/null &  
+    
   echo $line >>  /dev/shm/printer-events.log/printer-events.$(date -u  +%F_%H.%M);
 done 
